1ea637e456bfeeb998d8c8d64671406f1b6a4590
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-05-15 16:34:55 -0700

    added what I know of the fuction so far

---
+++ assets/javaScript/game.js
@@ -0,0 +1,18 @@
+ //this records user button press
+
+ document.onkeyup = function(event){
+
+    //determines which key was pressed
+
+    var userGuess = event.key;
+
+    //code to make computer randomly choose from array
+
+    var computerGuess = computerChoices [
+        Math.floor(math.random()*computerChoices.length)];
+
+console.log()
+
+
+        
+}
