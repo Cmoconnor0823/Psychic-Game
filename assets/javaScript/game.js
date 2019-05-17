927ccb2985c4f73ffa75630ae5ac9db49e9cf1e0
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-05-16 20:08:35 -0700

    filled out the functions in js

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -4,9 +4,8 @@
       //create variables for counts?
       var wins = 0;
       var losses = 0;
-      //do I need a var user guess ending with []
-      var fail = []; //need to be an empty array to push info to
-
+      var failGuess = []; 
+      var guessesLeft = 9;
       //var relating to html
 
       var directionsVan = document.getElementById ("directions-van");
@@ -15,21 +14,61 @@
       var lossesText = document.getElementById ("losses-text");
       var guessesLeft = document.getElementById ("guesses-left");
       var failGuess = document.getElementById ("fail-guess");
-      // rename this????    var computerGuess = document.getElementById ("computer-guess");
+      var computerGuess = document.getElementById ("computer-guess");
 
+        
  document.onkeyup = function(event){
-
-    //determines which key was pressed
+         //determines which key was pressed
 
     var userGuess = event.key;
+    console.log(userGuess)
 
     //code to make computer randomly choose from array
 
     var computerGuess = computerChoices [
-        Math.floor(math.random()*computerChoices.length)];
-
-console.log(userGuess)
+        Math.floor(Math.random()*computerChoices.length)];
 
 
+        function countGuessesLeft() {
+            document.getElementById("#guesses-left").innerHTML = "Guesses Left: " + guessesLeft;
+        }
+
+        function farUserGuesses() {
+            document.getElementById("#fail-guess").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
+        }
         
-}
+        countGuessesLeft();
+        
+        var restart = function() {
+            guessesLeft = 9;
+            failGuess = [];
+            var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        }
+    guessesLeft--;
+
+	userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	failguess.push(userGuess);
+	countGuessesLeft();
+	farUserGuesses();
+
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#win-text").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose-text").innerHTML = "Loses: " + losses;
+		restart();
+	}
+  };
+
+
+
+
+
+
+
+
+
