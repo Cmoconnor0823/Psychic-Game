a5a9ccfb8f6408cf956bea1c69bd9811594b2a49
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-05-18 16:57:55 -0700

    Lots o tweaks
    With TA help cleaned up missing or incomplete tags and syntax
    added working user guess recording and try countdown
    still missing:
    Win counter
    Restart at fail game

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -1,58 +1,83 @@
- //this records user button press
- var computerChoices = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//this records user button press
 
-      //create variables for counts?
-      var wins = 0;
-      var losses = 0; 
-      var guessCount = [10];
-      
-      //var to print to html
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-      var directionsVan = document.getElementById ("directions-van");
-      var directionsText = document.getElementById ("directions-text");
-      var winText = document.getElementById ("win-text");
-      var lossesText = document.getElementById ("losses-text");
-      var guessesLeft = document.getElementById ("guesses-left");
-      var failGuess = document.getElementById ("fail-guess");
-      var computerGuess = document.getElementById ("computer-guess");
+//create variables for counts?
+var wins = 0;
+var losses = 0;
+var userGuess = [];
+var guessesLeft = 9;
+var choicesToGuess = null;
+var failGuessArr = [];
 
-      //adding debugging attempt remove function
 
-      var guessesLeft = document.getElementById("guesses-left");
+//var to print to html
 
-      //code to make computer randomly choose from array
+var directionsVan = document.getElementById("directions-van");
+var directionsText = document.getElementById("directions-text");
+var winText = document.getElementById("win-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeft = document.getElementById("guesses-left");
+var failGuess = document.getElementById("fail-guess");
+//var computerGuess = document.getElementById ("computer-guess");
 
-      var computerGuess = computerChoices [
-        Math.floor(Math.random()*computerChoices.length)];
-      
-        ///in here lies the problem????
-      var restart = function() {
-          guessesLeft = 9;
-          failGuess = [];
-          var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-      }
-        
-    document.onkeyup = function(event){
-         //determines which key was pressed
 
-        var userGuess = event.key;
-        console.log(userGuess)
+///in here lies the problem????
 
-        
-        if (userGuess === computerGuess){
-		wins++;
-		document.querySelector("#win-text").innerHTML = "Wins: " + wins;
-		restart();
-	    } 
-        
-        else if (guessesLeft === 0) {
-		losses++;
-		document.querySelector("#lose-text").innerHTML = "Loses: " + losses;
-		restart();
-        }
-    
-        guessesLeft.textContent = "You have: " + (10 - guessCount.length) + " tries remaining.";
-    };
+var restart = function () {
+  userGuess = [];
+  guessCount = 9;
+  computerGuess();
+  guessesLeft = 9;
+  failGuessArr = [];
+}
+
+//code to make computer randomly choose from array
+var computerGuess = function () {
+  choicesToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+  console.log(choicesToGuess)
+
+}
+//code to print user guesses so far
+var userGuess = function () {
+  document.getElementById("fail-guess").innerHTML = "Guesses so far: " + userGuess.join(",");
+};
+
+computerGuess();
+
+
+document.onkeyup = function (event) {
+  //determines which key was pressed
+
+  var userGuess = event.key;
+  console.log(userGuess)
+
+
+  
+//wins do not record
+  if (userGuess === computerGuess) {
+    wins++;
+    document.querySelector("#win-text").innerHTML = "Wins: " + (wins) ;
+    //restart();
+    console.log(wins)
+  }
+   
+
+  else {
+    losses++;
+    document.querySelector("#losses-text").innerHTML = "Loses: " + (losses) ;
+
+    failGuessArr.push(userGuess);
+    failGuess.innerHTML = "Your Guesses so far: " + failGuessArr.join(",")
+    guessesLeft.textContent = "You have: " + (10 - failGuessArr.length) + " tries remaining.";
+
+}
+  //else {
+    //restart()
+
+ // }
+
+};
 
 
 
